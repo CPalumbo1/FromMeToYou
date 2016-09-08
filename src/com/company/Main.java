c07931721e6c801aeee8e6e2f81beaa8a0b6113b
CPalumbo1 <colton.palumbo@aol.com>
2016-09-08 17:32:39 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+//Author: Colton Palumbo
+//Date created: Sept 7, 2016
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+System.out.println("From: Bill Smith");
+        System.out.println("Dell Computer, Bldg 13");
+        System.out.println("Date: April 12, 2005");
+        System.out.println("");
+        System.out.println("To: Jack Jones");
+        System.out.println("");
+        System.out.println("Message: Help! I'm trapped inside a computer!");
+
+
+    }
+}
